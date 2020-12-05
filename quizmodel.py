98a1a8814e22ab9030bcc4b7fd6cbc202802656d
KaArTtHiK <40610132+KaArTtHiK@users.noreply.github.com>
2020-12-06 01:16:45 +0530

    Add files via upload
    A small quiz program using open() and modes

---
+++ quizmodel.py
@@ -0,0 +1,11 @@
+q_file = open("questions.txt", "r")
+q = [line.strip() for line in q_file.readlines()]
+q_file.close()
+n = 0
+for i in q:
+    q1, a1 = i.split("=")
+    a = input(f"{q1}=")
+    if a == a1:
+        n += 1
+m = len(q)
+print(f"Your final score is {n}/{m}")
